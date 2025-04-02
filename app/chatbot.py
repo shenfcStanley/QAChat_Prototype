29154f4367199209c7828a66c399d7ab5514d699
Feichen Shen <shenfeichen1102@gmail.com>
2025-04-02 11:31:34 -0700

    Update chatbot.py

--- app/chatbot.py
+++ app/chatbot.py
@@ -29,7 +29,7 @@ def shorten_sentence(text, max_words=30):
     words = text.split()
     return " ".join(words[:max_words]) + ("..." if len(words) > max_words else "")
 
-pdf_path = "data/HER2_Paper.pdf"
+pdf_path = "../data/HER2_Paper.pdf"
 processor = DataReader(chunk_size=1000, chunk_overlap=200)
 chunks = processor.load_and_split(pdf_path)
 
@@ -45,8 +45,8 @@ print(chunks[0].page_content)  # Example output
 
 ## Use pre-trained Clinical Knowledge Embeddings
 embedding_model = ClinicalKGEmbedding(
-        mapping_csv_path="clinical_KGEmb/new_node_map_df.csv",
-        embedding_pkl_path="clinical_KGEmb/full_h_embed_hms.pkl"
+        mapping_csv_path="../clinical_KGEmb/new_node_map_df.csv",
+        embedding_pkl_path="../clinical_KGEmb/full_h_embed_hms.pkl"
     )
 
 # vector db
@@ -56,7 +56,7 @@ print("Retriever:", retriever)
 
 llm = LlamaCpp(
     #model_path="models/llama-2-7b.Q4_K_M.gguf",
-    model_path="models/Nous-Hermes-2-Mistral-7B-DPO.Q4_K_M.gguf",
+    model_path="../models/Nous-Hermes-2-Mistral-7B-DPO.Q4_K_M.gguf",
     n_ctx=2048,
     temperature=0.1,
     max_tokens=512
